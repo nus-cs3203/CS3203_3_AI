eae04d05ff72730d1b4f6484fc4a13cc3d07d4fc
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2025-02-27 03:09:51 +0800

    Add example for sentiment clustering insights

---
+++ examples/example_insights_clusters.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from insight_generator.base_insight import BaseInsightGenerator
+from insight_generator.cluster_maker import SentimentClusteringDecorator
+
+# Sample Reddit posts
+df = pd.read_csv("files/sentiment_scored_2023_data.csv")
+
+# Apply detector
+base_generator = BaseInsightGenerator()
+cluster_detector = SentimentClusteringDecorator(base_generator)
+
+# Extract insights
+insights = cluster_detector.extract_insights(df)
